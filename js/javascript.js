601427fcf836c60e262d633d3dfb416db050349b
zhoujh30 <JianhaoZhou@Jianhaoacintosh.home>
2016-03-22 00:56:49 -0400

    Revise the sites and clean files that are not needed

---
+++ js/javascript.js
@@ -0,0 +1,26 @@
+$('#makeBlack').click(function() {
+  $('#myFirstDiv').css('color', 'black')
+  $('#myFirstDiv').css('background-color', '#cccccc');
+});
+
+$('#makeBlue').click(function() {
+  $('#myFirstDiv').css('color', 'blue'),
+  $('#myFirstDiv').css('background-color', '#bdd7e7');
+});
+
+$('#makeRed').click(function() {
+  $('#myFirstDiv').css('color', 'red'),
+  $('#myFirstDiv').css('background-color', '#fee5d9');
+});
+
+var myNumber = 36;
+
+$('#addOne').click(
+  function() {
+    myNumber = myNumber + 1;
+    $('#myNumber').text(myNumber);
+    alert('Thanks for your feedback!');
+  }
+)
+
+
